f69ca6a609a75cc39c2915a60f47c0ffb16538d3
Paola Aguilar <pao.aquev29@gmail.com>
2021-04-18 19:22:14 -0600

    💄 home styles added

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,27 +1,52 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+// /* eslint-disable global-require */
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import TicTacToeImg from '../../images/tictactoe.png';
+import TimeMachineImg from '../../images/timemachine.png';
 
-const Home = (): any => {
+import '../../styles/home.scss';
+
+const Home = (): JSX.Element => {
   const history = useHistory();
   return (
     <div>
-      <h1>CHOOSE YOUR GAME</h1>
-      <button
-        type="button"
-        onClick={() => {
-          history.push('/timeMachine');
-        }}
-      >
-        Time Machine
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          history.push('/ticTacToe');
-        }}
-      >
-        Tic Tac Toe
-      </button>
+      <h1 className="tittle">CHOOSE YOUR GAME</h1>
+      <div className="home">
+        <div className="home__select-game">
+          <img
+            src={TicTacToeImg}
+            alt="tictactoe"
+            onClick={() => {
+              history.push('/ticTacToe');
+            }}
+          />
+          <h2
+            onClick={() => {
+              history.push('/ticTacToe');
+            }}
+          >
+            Tic Tac Toe
+          </h2>
+        </div>
+        <div className="home__select-game">
+          <img
+            src={TimeMachineImg}
+            alt="timemachine"
+            onClick={() => {
+              history.push('/timeMachine');
+            }}
+          />
+          <h2
+            onClick={() => {
+              history.push('/timeMachine');
+            }}
+          >
+            Time Machine
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
